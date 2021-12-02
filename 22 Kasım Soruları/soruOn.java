336e23175aa84deef10a615608d0be453ebe1f4f
onur cetiner <93998298+cetineronur@users.noreply.github.com>
2021-12-02 22:03:50 +0100

    Update soruOn.java

--- 22 Kasım Soruları/soruOn.java
+++ 22 Kasım Soruları/soruOn.java
@@ -4,7 +4,7 @@ public class soruOn {
 
 	public static void main(String[] args) {
 		/* 1'den 100'e kadar olan doğal sayıların toplamını bulan programı yazınız. (Replit - For_while 10)
-OutPut:  Sayilarin Toplami : 5050  */
+OutPut:  Sayilarin Toplami : 5050ghgh  */
 
 
 	
